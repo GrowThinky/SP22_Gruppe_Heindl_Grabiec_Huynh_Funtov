b1a244fa32e322a917287dfd755e35aa2696f163
Christoph Heindl <christoph@Christophs-MacBook-Pro-2.local>
2022-07-13 14:56:36 +0200

    added Data and Login Tests

--- IliasApp/app/src/androidTest/java/de/uni_marburg/iliasapp/ModulSearchDataTest.java
+++ IliasApp/app/src/androidTest/java/de/uni_marburg/iliasapp/ModulSearchDataTest.java
@@ -3,12 +3,14 @@ package de.uni_marburg.iliasapp;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import de.uni_marburg.iliasapp.data.ModulSearchData;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +18,16 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ModulSearchDataTest {
     @Test
-    public void useAppContext() {
+    public void testModulSearchDataInstantiation() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("de.uni_marburg.iliasapp", appContext.getPackageName());
+        ModulSearchData data = new ModulSearchData(appContext);
+        Modul testModul = data.modulListe.get(0);
+        assertEquals(testModul.name,"Adaptive Numerische Verfahren für Operatorgleichungen");
     }
+
+
 }
