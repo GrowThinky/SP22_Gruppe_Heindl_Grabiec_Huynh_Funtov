9225f715c67c3273edeb5d9f1fbde43528715521
Christoph Heindl <christoph@swla2618.WLAN.Uni-Marburg.DE>
2022-07-28 19:48:30 +0200

    saveModuleTest

---
+++ IliasApp/app/src/androidTest/java/de/uni_marburg/iliasapp/login/SaveModuleTest.java
@@ -0,0 +1,137 @@
+package de.uni_marburg.iliasapp.login;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.uni_marburg.iliasapp.R;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SaveModuleTest {
+
+    @Rule
+    public ActivityScenarioRule<Login> mActivityScenarioRule =
+            new ActivityScenarioRule<>(Login.class);
+
+    @Test
+    public void saveModuleTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("test"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.login), withText("Sign in or register"),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.button_Veranstaltungsplan), withText("Veranstaltungsplan"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recyclerView),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                2)));
+        recyclerView.perform(actionOnItemAtPosition(30, click()));
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.button_Speichern), withText("Speichern"),
+                        childAtPosition(
+                                allOf(withId(R.id.veranstaltung_text),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                7),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        pressBack();
+
+        pressBack();
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.button_mVeranstaltungen), withText("Meine Veranstaltungen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.modul_name), withText("Datenbanksysteme"),
+                        withParent(withParent(withId(R.id.recyclerViewMV))),
+                        isDisplayed()));
+        textView.check(matches(withText("Datenbanksysteme")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
